ffec309155e478960488fc5977cd5497093ca8cc
chayanan102 <68525337+chayanan102@users.noreply.github.com>
2021-05-17 15:25:20 +0700

    อัพเดต 0.2

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -50,14 +50,13 @@
           <li><a class="nav-link scrollto active" href="#hero">Home</a></li>
           <li><a class="nav-link scrollto" href="#about">About</a></li>
           <li><a class="nav-link scrollto" href="#portfolio">Portfolio</a></li>
-          {{-- <li><a class="nav-link scrollto " href="#portfolio">Portfolio</a></li> --}}
           <li><a class="nav-link scrollto" href="#team">Team</a></li>
           <li><a class="nav-link scrollto" href="#clients">Our Client</a></li>
           <li><a class="nav-link scrollto" href="#contact">Contact</a></li>
 
           
         </ul>
-        <i class="bi bi-list mobile-nav-toggle"></i>
+       
       </nav><!-- .navbar -->
 
       <a href="#about" class="get-started-btn scrollto">Get Started</a>
